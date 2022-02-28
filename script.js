64fd766d7ffcee700ccfbde235cd722c24c7125c
EliiinaZ <forgameplay1122@gmail.com>
2022-02-28 15:19:25 +0200

    Fināls

--- script.js
+++ script.js
@@ -20,13 +20,58 @@ function perimetrs(m1, m2, m3) {
     return (p);
 }
 function pusper(m1, m2, m3) {
-    const pusper = perimetrs (m1 + m2 + m3) / 2;
+    const pusper = perimetrs(m1 + m2 + m3) / 2;
     console.log(pusper);
     return (pusper);
 }
-function laukums(m1,m2,m3){
-    const pusper = perimetrs(m1,m2,m3)/2;
-    let laukums = Math.sqrt( pusper* ((pusper-m1)*(pusper-m2)*(pusper-m3)));
-    console.log(laukums);
-    return (laukums);
+function laukums(m1, m2, m3) {
+    const pusper = perimetrs(m1, m2, m3) / 2;
+    let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
+    console.log(tlaukums);
+    return (tlaukums);
+}
+function irTrijsturis(m1, m2, m3) { //pārbauda vai eksist e trijstūris
+    //pārbauda vai izpildās trijstūra nevienādība
+    //atrgriež true vai flase
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function rezultats() {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()) {
+        t = "Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākām par 0";
+    }else {
+        if (irTrijsturis(m1,m2,m3)==false){
+            t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+        }else {
+            t="Trijsūtirs ar malu garumiem "+m1+", "+m2+" un "+m3+" eksistē."; 
+        if (m1==m2 && m2==m3){
+            t+="Tas ir vienādmalu trijstūris.";
+        }else {
+            if (m1==m2 || m2==m3 || m1==m3){
+                t+="Tas ir vienādsānu trijstūris."
+            }
+            const p=perimetrs(m1,m2,m3);
+            const s=Math.round(laukums(m1,m2,m3)*100)/100;
+            t+="Perimetrs ir" +p+ "un laukums ir "+s+ ".";
+        }
+        }
+        console.log(t);
+        return t;
+    }
+}
+function izvadaTekstu(){
+    //jāizvada funkcijas rezultāts
+    const teksts=rezultats();
+    //elementā ar id "izvade" izvada tekstu
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    sakne.innerHTML = teksts;
+    sakne.appendChild(raksti);
 }
